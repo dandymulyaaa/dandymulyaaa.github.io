35c58b6fd6d5921a5b48afefc1e3ed7141a0eedb
dandymulyaaa <dandymulya30@gmail.com>
2023-08-30 18:36:06 +0700

    ada lagi2

--- js/script.js
+++ js/script.js
@@ -34,3 +34,20 @@ buttonKirim.addEventListener('click', () => {
   updateDateNow();
 })
 
+const links = document.querySelectorAll('a[href^="#"]');
+  links.forEach(link => {
+    link.addEventListener("click", function(e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute("href"));
+      if (target) {
+        target.scrollIntoView({
+          behavior: "smooth"
+        });
+      }
+    });
+  });
+
+
+
+
+
